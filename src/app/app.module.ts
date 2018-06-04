ac2fdcae40f9ea7430688ed68e0fe3234a0970d1
a.pueblah <a.pueblah@alumnos.upm.es>
2018-06-04 16:43:59 +0200

    Add chart view and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './pages/common/nav-bar/nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
