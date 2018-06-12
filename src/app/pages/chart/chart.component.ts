52cbc2b53cc08a906aa730da490329974471da49
a.pueblah <a.pueblah@alumnos.upm.es>
2018-06-12 17:05:04 +0200

    Refactor chart component

--- src/app/pages/chart/chart.component.ts
+++ src/app/pages/chart/chart.component.ts
@@ -1,24 +1,42 @@
+import { Observable } from 'rxjs';
+import { AuthService } from './../../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { DataProviderService } from '../../services/data-provider/data-provider.service';
-import { User } from '../../models/user';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit{
+export class ChartComponent implements OnInit {
   chart: any;
-  constructor(public dpService: DataProviderService) { }
-  ngOnInit() {
-  
+  email: string;
+  userLogged: any;
+
+  constructor(
+    public dpService: DataProviderService,
+    public authService: AuthService,
+    public db: AngularFireDatabase,
+  ) {
+    this.authService.getAuth().subscribe(user => {
+      this.email = user.email;
+      console.log("Email", this.email);
+    })
+
+    this.userLogged = this.dpService.filterUserByEmail(this.email).valueChanges();
+    console.log("Usuario", this.userLogged);
   }
-setChart(chartType: any){
-  this.generateChart(chartType);
-}
-  generateChart(chartType: any){
-    if(this.chart !== undefined){
+
+  ngOnInit() { }
+
+  setChart(chartType: any) {
+    this.generateChart(chartType);
+  }
+  generateChart(chartType: any) {
+    if (this.chart !== undefined) {
       this.chart.destroy();
     }
     this.chart = new Chart('canvas', {
@@ -28,10 +46,11 @@ setChart(chartType: any){
         datasets: [{
           label: "Car Speed",
           data: [0, 59, 75, 20, 20, 55, 40],
-        }]},
+        }]
+      },
       options: {
-          responsive: true
+        responsive: true
       }
-  });
+    });
   }
 }
--- src/app/services/data-provider/data-provider.service.ts
+++ src/app/services/data-provider/data-provider.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from "rxjs/operators";
-import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+import { map, switchMap } from "rxjs/operators";
+import { AngularFireDatabase, AngularFireList, AngularFireObject, AngularFireAction } from 'angularfire2/database';
 
 
 @Injectable()
@@ -10,20 +10,22 @@ export class DataProviderService {
   users: Observable<any[]>;
   user: Observable<any>;
   public id: string;
-  usersRef: AngularFireList<any>
-  userRef: AngularFireObject<any>
+  usersRef: AngularFireList<any>;
+  userRef: AngularFireObject<any>;
+  users$: Observable<any>;
+  email$: BehaviorSubject<string | null>
 
   constructor(private db: AngularFireDatabase) {
     this.usersRef = db.list('users');
     this.users = db.list('users').valueChanges();
   }
 
-  addNewUser(idUser: string, user: any){
+  addNewUser(idUser: string, user: any) {
     this.id = idUser;
     this.usersRef.set(idUser, user);
   }
 
-  getAllUsers():Observable<any[]>{
+  getAllUsers(): Observable<any[]> {
     return this.users = this.usersRef.snapshotChanges().pipe(
       map(actions => actions.map(a => {
         const data = a.payload.val();
@@ -33,25 +35,44 @@ export class DataProviderService {
     );
   }
 
-  getOneUser(idUser: string){
+  getOneUser(idUser: string) {
     this.userRef = this.db.object(`users/${idUser}`);
     this.user = this.userRef.snapshotChanges().pipe(
       map(action => {
-          const data = action.payload.val();
-          data.id = action.key;
-          return  data ;
+        const data = action.payload.val();
+        data.id = action.key;
+        return data;
       })
     );
     return this.user;
   }
-  updateUser(user: any){
+  updateUser(user: any) {
     this.userRef = this.db.object(`users/${user.id}`);
     this.userRef.update(user);
   }
-  deleteUser(user: any){
+  deleteUser(user: any) {
     this.userRef = this.db.object(`users/${user.id}`);
     this.userRef.remove();
   }
+
+  /*getUserId(email) {
+    this.email$ = new BehaviorSubject(null);
+    this.users$ = this.email$.pipe(
+      switchMap(userEmail =>
+        this.db.list('/users', ref =>
+          userEmail ? ref.orderByChild('email').equalTo(email) : ref
+        ).snapshotChanges()
+      )
+    );
+
+    this.email$.next(email);
+  }*/
+
+  filterUserByEmail(email: string|null) {
+    console.log(this.db.list('/users', ref => ref.orderByChild('email').equalTo(email)))
+    return this.db.list('/users', ref => ref.orderByChild('email').equalTo(email)) ;
+  }
+
 }
 
 
